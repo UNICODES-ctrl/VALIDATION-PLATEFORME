d30691fb6e5b4a57c5d98fdaf452895d9004c3fe
daniel23-crtl <dannyakoumany@gmail.com>
2026-05-02 23:08:01 +0000

    end 3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import path from "path"
 
 const nextConfig: NextConfig = {
-  /* Supprime le bloc turbopack ici */
-};
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+}
 
-export default nextConfig;
+export default nextConfig
